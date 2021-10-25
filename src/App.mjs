94b7cf475af38cfb2f058327e77c5ad5ec731f16
Oliver Nordbjerg <hi@notbjerg.me>
2021-10-26 01:27:43 +0200

    Initial

---
+++ src/App.mjs
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App () {
+  return 'welcome to the range sers'
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
